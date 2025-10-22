09b87212dd51a8a0255f3bcb971440f280c39e50
mohammadyamankreidi-jpg <mohammadyamankreidi@gmail.com>
2025-10-22 10:49:09 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDSiAvpmm7E4M9RQz7EWWCaTNwFUiIkKHY",
+  authDomain: "shiftstart-e0de1.firebaseapp.com",
+  projectId: "shiftstart-e0de1",
+  storageBucket: "shiftstart-e0de1.appspot.com",
+  messagingSenderId: "813927560829",
+  appId: "1:813927560829:web:664de447ca9fd52428e314"
+};
